66057ac77dceef5af0b519202cec8750fe62291a
Chris Cardle <cdcardle@gmail.com>
2018-07-19 13:20:41 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,7 +2,7 @@ module ArtistsHelper
 
 def display_artist(song)
   if song.artist
-    link_to artist.name, artist_path(artist)
+    link_to song.artist.name, artist_path(artist)
   else
     "Failed"
   end
